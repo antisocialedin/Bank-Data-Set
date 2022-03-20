261a80c8b48388b12d1d1368f5973cb34f16ca4b
Eder Filho <eder.filho@aluno.ifsp.edu.br>
2022-03-20 16:31:35 -0300

    Conversão das tabelas categóricas para numéric
    DataConvertion.py
    
    Co-Authored-By: Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>

--- 1-Preprocessing/DataConvertion.py
+++ 1-Preprocessing/DataConvertion.py
@@ -1,37 +1,26 @@
 import pandas as pd
-
-# from sklearn.preprocessing import MinMaxScaler
+#from sklearn.preprocessing import MinMaxScaler
 
 def main():
-  input_file = '0-Datasets/bankClear.csv'
+  input_file = '../0-Datasets/bankClear.csv'
   output_file = '../0-Datasets/bankConvert.csv'
   names = ['age','job','marital','education','default','balance','housing','loan','contact','day','month','duration','campaign','pdays','previous','poutcome','y'] 
+  features = ['age','job','marital','education','default','balance','housing','loan','contact','day','month','duration','campaign','pdays','previous','poutcome','y']
   
-  df = pd.read_csv(input_file,          # Nome do arquivo com dados
+  df = pd.read_csv(  input_file,          # Nome do arquivo com dados
                      names = names,       # Nome das colunas 
+                     usecols = features,  # Define as colunas que serão utilizadas
                      na_values='unknown')
   print(df.head(10))
 
   #identify all categorical variables
   cat_columns = df.select_dtypes(['object']).columns
-  print (cat_columns)
+  
   #convert all categorical variables to numeric
   df[cat_columns] = df[cat_columns].apply(lambda x: pd.factorize(x)[0])
   print(df.head(10))
-  
-"""   #Discretização -> simbolos numéricos para números
-  df = df.apply(lambda x: pd.factorize(x, sort=True)[0])
-  
-  #Normalização Min-Max
-  x = df.loc[:, features].values
-  x_minmax = MinMaxScaler().fit_transform(x)
-  normalizedDf = pd.DataFrame(data=x_minmax, columns=features)
-  normalizedDf = pd.concat([normalizedDf, df[[target]]], axis=1)
-  print(normalizedDf.info())
-  print(normalizedDf.describe())
-  print(normalizedDf.head(15))
 
-  normalizedDf.to_csv(output_file, header=False, index=False) """
+  df.to_csv(output_file, header=False, index=False) 
 
 if __name__ == "__main__":
   main()
