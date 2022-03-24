ab19dc5786d623517084f6acf9eeb69720a196a1
Eder Filho <eder.filho@aluno.ifsp.edu.br>
2022-03-24 17:04:54 -0300

    PCA e Distribuição de Frequência
    Co-Authored-By: Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>

--- 1-Preprocessing/DataReduction3D.py
+++ 1-Preprocessing/DataReduction3D.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
-def main():
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.decomposition import PCA
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+# %matplotlib notebook
+
+def main():    
     # Faz a leitura do arquivo
     input_file = '../0-Datasets/bankNormal.csv'
     names = ['age',
@@ -42,14 +46,16 @@ def main():
                 'poutcome']
     target = 'y'
     df = pd.read_csv(input_file,    # Nome do arquivo com dados
-                     names = names) # Nome das colunas                      
+                        names = names) # Nome das colunas                      
     ShowInformationDataFrame(df,"Dataframe original")
-
-    # Separating out the features
+            
     x = df.loc[:, features].values
 
-    # Separating out the target
     y = df.loc[:,[target]].values
+    
+    y = df.loc[:,1].values
+
+    sc = StandardScaler()
 
     # Standardizing the features
     x = StandardScaler().fit_transform(x)
@@ -64,35 +70,37 @@ def main():
     print(pca.explained_variance_ratio_.tolist())
     print("\n\n")
 
-    principalDf = pd.DataFrame(data = principalComponents[:,0:2], 
+    principalDf = pd.DataFrame(data = principalComponents[:,0:3], 
                                columns = ['principal component 1', 
-                                          'principal component 2'])
+                                          'principal component 2',
+                                          'principal component 3'])
     finalDf = pd.concat([principalDf, df[[target]]], axis = 1)    
     ShowInformationDataFrame(finalDf,"Dataframe PCA")
     
     VisualizePcaProjection(finalDf, target)
 
-
+    
 def ShowInformationDataFrame(df, message=""):
     print(message+"\n")
     print(df.info())
     print(df.describe())
     print(df.head(10))
     print("\n")
-    
-           
+
 def VisualizePcaProjection(finalDf, targetColumn):
     fig = plt.figure(figsize = (8,8))
     ax = fig.add_subplot(1,1,1) 
     ax.set_xlabel('Principal Component 1', fontsize = 15)
     ax.set_ylabel('Principal Component 2', fontsize = 15)
-    ax.set_title('2 component PCA', fontsize = 20)
+    ax.set_zlabel('Principal Component 3', fontsize = 15)
+    ax.set_title('3 component PCA', fontsize = 20)
     targets = [0, 1, ]
-    colors = ['r', 'g']
+    colors = ['r', 'g', 'b']
     for target, color in zip(targets,colors):
         indicesToKeep = finalDf[targetColumn] == target
         ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1'],
                    finalDf.loc[indicesToKeep, 'principal component 2'],
+                   finalDf.loc[indicesToKeep, 'principal component 3'],
                    c = color, s = 50)
     ax.legend(targets)
     ax.grid()
--- 2-Clustering/DataFreq.py
+++ 2-Clustering/DataFreq.py
@@ -4,30 +4,40 @@ import numpy as np
 
 def main():
     input_file = '../0-Datasets/bankNormal.csv'
-    names = ['age',
-            'job',
-            'marital',
-            'education',
-            'default',
-            'balance',
-            'housing',
-            'loan',
-            'contact',
-            'day',
-            'month',
-            'duration',
-            'campaign',
-            'pdays',
-            'previous',
-            'poutcome',
-            'y']
+    names = ['age']
     df = pd.read_csv(input_file, 
                     names=names, 
                     na_values='?')
-    df = df.apply(lambda x: pd.factorize(x, sort=True)[0])
+    #df = df.apply(lambda x: pd.factorize(x, sort=True)[0])
+    
+    df.sort_values(ascending=True)
+    
+    at = df.max() - df.min()
+    
+    # Lembrando que k = raiz quadrada do total de registros/amostras
+    k = math.sqrt(len(df))
+    # O valor de amplitude de classe pode ser arredondado para um número inteiro, geralmente para facilitar a interpretação da tabela.
+    h = at/k 
+    h = math.ceil(h)
+    
+    frequencias = []
 
+    # Menor valor da série
+    menor = round(df.min(),1)
+
+    # Menor valor somado a amplitude
+    menor_amp = round(menor+h,1)
+
+    valor = menor
+    while valor < df.max():
+    frequencias.append('{} - {}'.format(round(valor,1),round(valor+h,1)))
+    valor += h
+    
+    freq_abs = pd.qcut(df,len(frequencias),labels=frequencias) # Discretização dos valores em k faixas, rotuladas pela lista criada anteriormente
+    print(pd.value_counts(freq_abs))
+    
     """ # Distribuição de frequência de cap-shape
-    cap_shape = df['cap-shape']
+    cap_shape = df['age']
     labels = ['bell','conical','flat','sunken','knobbed','convex']
     sizes, x = np.histogram(cap_shape, bins=np.arange(7))
     plt.pie(sizes, labels=labels)
@@ -41,25 +51,10 @@ def main():
     plt.show() """
 
     # Distribuição de frequência de y
-    y = df['y']
-    labels =  ['age',
-            'job',
-            'marital',
-            'education',
-            'default',
-            'balance',
-            'housing',
-            'loan',
-            'contact',
-            'day',
-            'month',
-            'duration',
-            'campaign',
-            'pdays',
-            'previous',
-            'poutcome']
-    sizes, x = np.histogram(y, bins=np.arange(17))
-    plt.bar(np.arange(16), height=sizes, tick_label=labels)
+    y = freq_abs['age']
+    labels =  ['age']
+    sizes, x = np.histogram(y, bins=np.arange(2))
+    plt.bar(np.arange(10), height=sizes, tick_label=labels)
     plt.show()
 
 if __name__ == "__main__":
