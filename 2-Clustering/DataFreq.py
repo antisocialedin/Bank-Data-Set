0f73b4a87d50b6641893d5c54b0aab0269d4d542
Eder Filho <eder.filho@aluno.ifsp.edu.br>
2022-03-29 15:01:56 -0300

    DataFreq teste histograma
    Co-Authored-By: Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>

--- 2-Clustering/DataFreq.py
+++ 2-Clustering/DataFreq.py
@@ -1,61 +1,64 @@
+import math
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    input_file = '../0-Datasets/bankNormal.csv'
-    names = ['age']
-    df = pd.read_csv(input_file, 
-                    names=names, 
-                    na_values='?')
-    #df = df.apply(lambda x: pd.factorize(x, sort=True)[0])
-    
-    df.sort_values(ascending=True)
-    
-    at = df.max() - df.min()
-    
-    # Lembrando que k = raiz quadrada do total de registros/amostras
-    k = math.sqrt(len(df))
-    # O valor de amplitude de classe pode ser arredondado para um número inteiro, geralmente para facilitar a interpretação da tabela.
-    h = at/k 
-    h = math.ceil(h)
-    
-    frequencias = []
+        input_file = '../0-Datasets/bankNormal.csv'
+        names = ['age']
+        df = pd.read_csv(input_file, 
+                        names=names, 
+                        na_values='?')
 
-    # Menor valor da série
-    menor = round(df.min(),1)
+        #df = df.apply(lambda x: pd.factorize(x, sort=True)[0])
 
-    # Menor valor somado a amplitude
-    menor_amp = round(menor+h,1)
+        #df.sort_values(ascending=True)
+        df.sort_values(by=['age']) 
 
-    valor = menor
-    while valor < df.max():
-    frequencias.append('{} - {}'.format(round(valor,1),round(valor+h,1)))
-    valor += h
-    
-    freq_abs = pd.qcut(df,len(frequencias),labels=frequencias) # Discretização dos valores em k faixas, rotuladas pela lista criada anteriormente
-    print(pd.value_counts(freq_abs))
-    
-    """ # Distribuição de frequência de cap-shape
-    cap_shape = df['age']
-    labels = ['bell','conical','flat','sunken','knobbed','convex']
-    sizes, x = np.histogram(cap_shape, bins=np.arange(7))
-    plt.pie(sizes, labels=labels)
-    plt.show() """
+        at = df.max() - df.min()
 
-    """ # Distribuição de frequência de odor
-    odor = df['odor']
-    labels =  ['almond','creosote','foul','anise','musty','none','pungent','spicy','fishy']
-    sizes, x = np.histogram(odor, bins=np.arange(10))
-    plt.bar(np.arange(9), height=sizes, tick_label=labels)
-    plt.show() """
+        # Lembrando que k = raiz quadrada do total de registros/amostras
+        k = math.sqrt(len(df))
+        # O valor de amplitude de classe pode ser arredondado para um número inteiro, geralmente para facilitar a interpretação da tabela.
+        h = at/k 
+        h = math.ceil(h)
 
-    # Distribuição de frequência de y
-    y = freq_abs['age']
-    labels =  ['age']
-    sizes, x = np.histogram(y, bins=np.arange(2))
-    plt.bar(np.arange(10), height=sizes, tick_label=labels)
-    plt.show()
+        frequencias = []
+
+        # Menor valor da série
+        menor = round(df.min(),1)
+
+        # Menor valor somado a amplitude
+       # menor_amp = round(menor+h,1)
+
+        valor = menor
+        while valor < df.max():
+                frequencias.append('{} - {}'.format(round(valor,1),round(valor+h,1)))
+                valor += h
+
+        freq_abs = pd.qcut(df,len(frequencias),labels=frequencias) # Discretização dos valores em k faixas, rotuladas pela lista criada anteriormente
+        print(pd.value_counts(freq_abs))
+
+        """ # Distribuição de frequência de cap-shape
+        cap_shape = df['age']
+        labels = ['bell','conical','flat','sunken','knobbed','convex']
+        sizes, x = np.histogram(cap_shape, bins=np.arange(7))
+        plt.pie(sizes, labels=labels)
+        plt.show() 
+
+        # Distribuição de frequência de odor
+        odor = df['odor']
+        labels =  ['almond','creosote','foul','anise','musty','none','pungent','spicy','fishy']
+        sizes, x = np.histogram(odor, bins=np.arange(10))
+        plt.bar(np.arange(9), height=sizes, tick_label=labels)
+        plt.show()  """
+
+        #Distribuição de frequência de y
+        y = df['age']
+        labels =  ['age']
+        sizes, x = np.histogram(y, bins=np.arange(11))
+        plt.bar(np.arange(10), height=sizes, tick_label=labels)
+        plt.show()
 
 if __name__ == "__main__":
-    main()
+        main()
