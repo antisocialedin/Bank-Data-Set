67a8334f362eeb30c3b81b7daa64dfc40d1ce790
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2022-03-30 22:16:37 -0300

    Medidas de Resumo
    Co-Authored-By: Eder Filho <eder.filho@aluno.ifsp.edu.br>

---
+++ 4-Regression/MedTendCent.py
@@ -0,0 +1,85 @@
+#DATADESCRIPTIVE
+import pandas as pd
+import numpy as np
+
+def main():
+    # Faz a leitura do arquivo
+    input_file = '../0-Datasets/bankNormal.csv'
+    names = ['age',
+            'job',
+            'marital',
+            'education',
+            'default',
+            'balance',
+            'housing',
+            'loan',
+            'contact',
+            'day',
+            'month',
+            'duration',
+            'campaign',
+            'pdays',
+            'previous',
+            'poutcome',
+            'y']    
+    features  = ['age','balance','loan'] # Define as colunas que serão  utilizadas
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                    usecols = features,
+                    names = names) # Nome das colunas   
+  
+    #media 
+    print('Média')
+    print(df.mean())
+    print('\n\n')
+
+    #median
+    print('Mediana')
+    print(df.median())
+    print('\n\n')
+
+    #quatil
+    print('Quantil')
+    print(df.quantile())
+    print('\n')
+    print('Quantil 25%')
+    print(df.quantile(q=0.25))
+    print('\n\n')
+
+    #moda
+    print('Moda')
+    print(df.mode())
+    print('\n\n')
+
+
+    #Medidas de dispersão
+    # Amplitude
+    print('Amplitude')
+    ampl = df.max() - df.min()
+    print(ampl)
+    print('\n\n')
+
+    #Variância
+    print('Variância')
+    print(df.var())
+    print('\n\n')
+
+    #Desvio Padrão
+    print('Desvio padrão')
+    print(df.std())
+    print('\n\n')
+
+    #Desvio absoluto
+    print('Desvio absoluto')
+    print(df.mad())
+    print('\n\n')
+
+    #Covariância e Correlação
+    print('Covariância')
+    print(df.cov())
+    print('\n')
+    print('Correlação')
+    print(df.corr())
+    print('\n')
+
+if __name__ == "_main_":
+    main()
