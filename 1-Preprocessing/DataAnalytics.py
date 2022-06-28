ce52f7e8d9e2a44f12926e61fcbc191f603aa426
Eder Filho <eder.filho@aluno.ifsp.edu.br>
2022-06-28 15:32:01 -0300

    DataFreqCorr.py
    Co-Authored-By: Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>

---
+++ 1-Preprocessing/DataAnalytics.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+
+
+def main():
+    # Faz a leitura do arquivo
+    input_file = '0-Datasets/bankConvert.csv'
+    names = ['age','job','marital','education','default','balance','housing','loan','y']
+    features = ['age','job','marital','education','default','balance','housing','loan']
+    target = 'y'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                    usecols = features,
+                     names = names) # Nome das colunas
+
+    #media 
+    print('Média')
+    print(df.mean())
+    print('\n\n')
+
+    #median
+    print('Mediana')
+    print(df.median())
+    print('\n\n')
+
+    #quatil
+    """ print('Quantil')
+    print(df.quantile())
+    print('\n') """
+    print('Quantil 25%')
+    print(df.quantile(q=0.25))
+    print('\n')
+    print('Quantil 75%')
+    print(df.quantile(q=0.75))
+    print('\n\n')
+
+    #moda
+    print('Moda')
+    print(df.mode())
+    print('\n\n')
+
+
+    #Medidas de dispersãp
+    # Amplitude
+    print('Amplitude')
+    ampl = df.max() - df.min()
+    print(ampl)
+    print('\n\n')
+
+    #Variância
+    print('Variância')
+    print(df.var())
+    print('\n\n')
+
+    #Desvio Padrão
+    print('Desvio padrão')
+    print(df.std())
+    print('\n\n')
+
+    #Desvio absoluto
+    print('Desvio absoluto')
+    print(df.mad())
+    print('\n\n')
+
+    #Covariância e Correlação
+    print('Covariância')
+    print(df.cov())
+    print('\n')
+    print('Correlação')
+    print(df.corr())
+    print('\n')
+
+if __name__ == "__main__":
+    main()
---
+++ 1-Preprocessing/DataFreqCorr.py
@@ -0,0 +1,51 @@
+from math import ceil
+from tracemalloc import stop
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+def main():
+    # Faz a leitura do arquivo
+    input_file = '0-Datasets/bankConvert.csv'
+    names = ['age','job','marital','education','default','balance','housing','loan','y']
+    features = ['age','job','marital','education','default','balance','housing','loan']
+    target = 'y'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                    usecols = features,
+                     names = names) # Nome das colunas
+
+    """ ################### IDADE X SALDO #####################
+    plt.subplots(figsize=(10, 5))
+    plt.scatter(df['age'],df['balance'])
+    plt.xlabel('idade')
+    plt.ylabel('saldo')
+    plt.title("Idade X Saldo")
+    plt.show() """
+
+    ################### EMPREGO X SALDO #####################
+    plt.subplots(figsize=(10, 5))
+    plt.scatter(df['job'],df['balance'])
+    plt.xlabel('job')
+    plt.ylabel('saldo')
+    plt.title("Emprego X Saldo")
+    plt.show()
+
+    ################### IDADE X EMPRESTIMO RESIDENCIAL #####################
+    plt.subplots(figsize=(10, 5))
+    plt.scatter(df['housing'],df['age'])
+    plt.xlabel('Empréstimo Residencial')
+    plt.ylabel('Idade')
+    plt.title("Emprestimo Residêncial X Idade")
+    plt.show()
+
+    ################### IDADE X EMPRESTIMO #####################
+    plt.subplots(figsize=(10, 5))
+    plt.scatter(df['loan'],df['age'])
+    plt.xlabel('Empréstimo')
+    plt.ylabel('Idade')
+    plt.title("Empréstimo X Idade")
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
